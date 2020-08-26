4991ea826a854751b27d86af24c6a4e925f7ef23
Alexey Malyshev <lordniken@mail.ru>
2020-08-26 04:40:05 +0300

    Updates

---
+++ precache-manifest.1336528f56a376b251ed609b22b9601b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fabc1c7c5089b29dca963dcdc99397c5",
+    "url": "/feed-your-cat/index.html"
+  },
+  {
+    "revision": "52f816172bed284e08b2",
+    "url": "/feed-your-cat/static/css/main.0c7af3f7.chunk.css"
+  },
+  {
+    "revision": "e4570db6613119464675",
+    "url": "/feed-your-cat/static/js/2.fc98474d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/feed-your-cat/static/js/2.fc98474d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52f816172bed284e08b2",
+    "url": "/feed-your-cat/static/js/main.fada402e.chunk.js"
+  },
+  {
+    "revision": "1b4229e0bee0eafe71cd",
+    "url": "/feed-your-cat/static/js/runtime-main.2956604e.js"
+  },
+  {
+    "revision": "e1ed0c4523954a41b255a96832ca9656",
+    "url": "/feed-your-cat/static/media/bg.e1ed0c45.png"
+  },
+  {
+    "revision": "7ca9c276aa8035c2095619c136a3abcc",
+    "url": "/feed-your-cat/static/media/cat.7ca9c276.jpg"
+  }
+]);
